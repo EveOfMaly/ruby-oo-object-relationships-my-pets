3d7b73046796459ba7959d5c590bd2af4970a772
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-10-26 11:42:52 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,18 +31,60 @@ class Owner
   end
 
   def cats
-    collection = []
+    Cat.all.select {|cat| cat.owner == self}
+  end
 
-     Cat.all.each do |cat| 
-      if cat.owner = self.name
-        collection << cat
-      end
-      collection
-    end
-  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(cat_name)
+    new_instance = Cat.new(cat_name, self)
   end
   
+  def buy_dog(dog_name)
+    new_instance = Dog.new(dog_name, self)
+  end
     
+  def walk_dogs
+    #sets the dog the owner is walking mood to happy
+    Dog.all.select do |dog|
+      if dog.owner == self 
+        dog.mood = "happy"
+      end
+    end
+  end
+
+  def feed_cats 
+    Cat.all.select do |cat|
+      if cat.owner == self 
+        cat.mood = "happy"
+      end
+    end
+  end
+
+
+  def sell_pets
+    Cat.all.select do |cat|
+      if cat.owner == self 
+        cat.mood = "nervous"
+        cat.owner = nil
+    
+      end
+    end
+
+    Dog.all.select do |dog|
+      if dog.owner == self 
+        dog.mood = "nervous"
+        dog.owner = nil
+      end
+    end
+
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 
 end
 
