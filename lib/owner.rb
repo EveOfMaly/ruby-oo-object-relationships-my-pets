6ad8b365379f3a613eee46a9cba183bc5da9709f
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-10-26 10:25:20 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :cats, :dogs
+
+  @@all  = []
+
+  def self.all 
+    @@all 
+  end
+
+  def initialize(owner_name)
+    @name = owner_name
+    @species = "human"
+    @@all << self 
+    @cats = []
+    @dogs = []
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.count 
+    self.all.count
+  end
+
+  def self.reset_all
+    @@all.clear 
+  end
+
+  def cats
+    collection = []
+
+     Cat.all.each do |cat| 
+      if cat.owner = self.name
+        collection << cat
+      end
+      collection
+    end
+  
+  end
+  
+    
+
 end
+
